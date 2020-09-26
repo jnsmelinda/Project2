3d5d7585bec732cdb995fcf38716fa3f65134a9b
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-26 13:51:28 -0700

    location to coordinates

--- aqi.js
+++ aqi.js
@@ -1,6 +1,8 @@
 let lat = 47.6062;
 let lon = -122.3321;
 
+const location = "Seattle";
+
 var fetchUrl = require("fetch").fetchUrl;
 
 // source file is iso-8859-15 but it is converted to utf-8 automatically
@@ -10,3 +12,14 @@ fetchUrl(
         console.log(JSON.parse(body)[0].AQI);
     }
 );
+
+placeToCoordinates(location);
+
+function placeToCoordinates(location) {
+    fetchUrl(
+        `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=9bdca107dee44c8d90c4efabb9b500e4`,
+        function(error, meta, body){
+            console.log(JSON.parse(body).results[0].geometry);
+        }
+    );
+}
