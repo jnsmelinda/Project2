6209ede36b089f00171d0bdeff780bd7d9cf74fe
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-28 20:44:07 -0700

    adding aqi api
    (cherry picked from commit dff3a7d35a192c629ec5d228689273f929567f37)

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,14 @@
 const db = require('../models');
+const getAQIByLocation = require('../aqi.js');
 
-// Routes
-
+module.exports = function(app) {
+    app.get("/api/aqi/:location", function(req, res, next) {
+        getAQIByLocation(
+            req.params.location,
+            (error, aqi) => {
+                if (error) next(error);
+                else res.json({aqi});
+            }
+        );
+    });
+}
