c0f26a37684e672afcce3d32ed29c2caf637f92a
Melinda Ivanov <jns.melinda@gmail.com>
2020-09-30 21:20:41 -0700

    errorhandling for coordinates

--- aqi.js
+++ aqi.js
@@ -1,25 +1,34 @@
 const fetchUrl = require("fetch").fetchUrl;
 
-function getAQIByLocation(location, callback) {
+function getAQIByLocation(location, callback, next) {
     fetchUrl(
         `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=9bdca107dee44c8d90c4efabb9b500e4`,
-        function(error, meta, body) {
+        (error, meta, body) => {
             const latitude = JSON.parse(body).results[0].geometry.lat;
             const longitude = JSON.parse(body).results[0].geometry.lng;
             fetchUrl(
                 `https://airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=${latitude}&longitude=${longitude}&distance=25&API_KEY=CB278A15-984D-44A7-A2DE-AA530CDDFD69`,
-                function(error, meta, body) {
-                    const response = {
-                        aqi: JSON.parse(body)[0].AQI,
-                        latitude: latitude,
-                        longitude: longitude
-                    };
-                    callback(error, response);
+                (error, meta, body) => {
+                    if (error) {
+                        next(error);
+                    }
+                    else if (meta.status !== 200) {
+                        const error = new Error(JSON.stringify(meta));
+                        error.status = meta.status;
+                        next(error);
+                    }
+                    else {
+                        const response = {
+                            aqi: JSON.parse(body)[0].AQI,
+                            latitude: latitude,
+                            longitude: longitude
+                        };
+                        callback(response);
+                    }
                 }
             );
         }
     );
 }
 
-
-module.exports = getAQIByLocation
+module.exports = getAQIByLocation;
