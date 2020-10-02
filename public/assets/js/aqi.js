32f9137e00fe591b42f8ac88706ced5c1e4155d9
Melinda Ivanov <jns.melinda@gmail.com>
2020-10-01 23:55:43 -0700

    setting the location back to the input

--- public/assets/js/aqi.js
+++ public/assets/js/aqi.js
@@ -2,6 +2,7 @@ $(document).ready(getAQI);
 
 function getAQI() {
     const location = $("#location").val().trim() || "Seattle";
+    $("#location").val(location);
     $.ajax({url: `/api/aqi/${location}`}).then(
         function(response) {
             const event = new CustomEvent('getAQI', {detail: response});
