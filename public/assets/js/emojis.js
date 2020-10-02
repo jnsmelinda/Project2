31fb48760d1e9b0146ad68251371d45946d439de
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-10-01 18:10:45 -0700

    Updating JS Files

--- public/assets/js/emojis.js
+++ public/assets/js/emojis.js
@@ -1,12 +1,20 @@
 function displayEmojiValue() {
     var ele = document.getElementsByName('smiley');
-
     for(i = 0; i < ele.length; i++) {
         if(ele[i].checked)
         document.getElementById("result").innerHTML
                 = ele[i].value;
-
                 console.log(result)
     }
 }
+
+function displayBreatheValue() {
+    var ele = document.getElementsByName('breathe');
+    for(i = 0; i < ele.length; i++) {
+        if(ele[i].checked)
+        document.getElementById("result2").innerHTML
+                = ele[i].value;
+                console.log(result2)
+    }
+};
 
