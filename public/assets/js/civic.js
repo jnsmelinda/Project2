6812b28ab6ec58f95196b4a8feda77897739a6ef
Wilbur125 <matt.s.wilbur@gmail.com>
2020-10-03 12:57:02 -0700

    Matt - debug api call for civic.js

--- public/assets/js/civic.js
+++ public/assets/js/civic.js
@@ -1,45 +1,51 @@
-      // displayCivicInfo function re-renders HTML
-      function displayCivicInfo() {
+$(document).ready(displayCivicInfo);
 
-        const location = // need path to user's input for location
-        const queryURL = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=" + location + "&includeOffices=true&roles=legislatorUpperBody&key=[AIzaSyBI6hpyHbsOD7Qk2e2C3d0Ti5kLW59QPc4]";
+function displayCivicInfo() {
+    const location = $("#location").val().trim().toLowerCase() || "seattle";
 
-        // creates an AJAX call for when the location is entered by the user
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
+    const queryURL = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=" + location + "&includeOffices=true&roles=legislatorUpperBody&key=AIzaSyBI6hpyHbsOD7Qk2e2C3d0Ti5kLW59QPc4";
 
-          const civicDiv = $("<div class='civic'>");
+    // creates an AJAX call for when the location is entered by the user
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        const civicDiv = $("<div class='civic'>");
 
-          // stores the politician's name and creates an element for their name
-          const civicName = response.officials.name;
-          const lineOne = $("<h5>").text("Name: " + civicName);
+        // stores the politician's name and creates an element for their name
+        const civicName = response.officials.name;
+        const lineOne = $("<h5>").text("Name: " + civicName);
 
-          // append the politician's name
-          civicDiv.append(lineOne);
+        // append the politician's name
+        civicDiv.append(lineOne);
 
-          // stores the politician's title and creates an element for title
-          const civicTitle = response.offices.name;
-          const lineTwo = $("<p>").text("Office: " + civicTitle);
+        // stores the politician's title and creates an element for title
+        const civicTitle = response.offices.name;
+        const lineTwo = $("<p>").text("Office: " + civicTitle);
 
-          // append the title
-          civicDiv.append(lineTwo);
+        // append the title
+        civicDiv.append(lineTwo);
 
-          // stores the party affiliation and creates an element for party
-          var civicParty = response.officials.party;
-          var lineThree = $("<p>").text("Party: " + civicParty);
+        // stores the party affiliation and creates an element for party
+        var civicParty = response.officials.party;
+        var lineThree = $("<p>").text("Party: " + civicParty);
 
-          // append the party
-          movieDiv.append(lineThree);
+        // append the party
+        movieDiv.append(lineThree);
 
-          // stores phone number and creates an element for the phone number
-          var civicPhone = response.officials.party;
-          var lineThree = $("<p>").text("Ph: " + civicPhone);
+        // stores phone number and creates an element for the phone number
+        var civicPhone = response.officials.party;
+        var lineThree = $("<p>").text("Ph: " + civicPhone);
 
-          // append the phone number
-          movieDiv.append(lineFour);
+        // append the phone number
+        movieDiv.append(lineFour);
 
-        });
+    });
+}
 
-      }
+displayCivicInfo();
+
+
+
+
